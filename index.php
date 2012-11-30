1c79f22d8383a95a589148fd8a249fcb42bf1ac7
Galen Gidman <galen@galengidman.com>
2012-11-29 23:23:37 -0600

    messed up </script> tag

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ function is_live() {
 	<?php
 
 		if ( is_live() ) {
-			echo '<script>var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-23917942-3"]);_gaq.push(["_trackPageview"]);(function(){var e=document.createElement("script");e.type="text/javascript";e.async=true;e.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})()</body>';
+			echo '<script>var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-23917942-3"]);_gaq.push(["_trackPageview"]);(function(){var e=document.createElement("script");e.type="text/javascript";e.async=true;e.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})()</script>';
 		}
 
 	?>
